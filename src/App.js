366e1792cbf9324de0a78eb4f81b64fb3f89f05c
Evgenia Filatova <silvochka07@gmail.com>
2023-12-03 17:49:33 -0800

    implement going back to main meny

--- src/App.js
+++ src/App.js
@@ -5,24 +5,40 @@ import GuideTile from "./views/GuideTile";
 import GuideView from "./views/GuideView";
 
 function App() {
-  const onClick = (guide) => {
+  const onItemSelected = (guide) => {
     setSelectedGuide(guide);
+    history.unshift(guide);
+    setHistory(history);
+  };
+
+  const onBack = () => {
+    history.shift();
+    setHistory(history);
+    setSelectedGuide(history[0]);
   };
 
   const [selectedGuide, setSelectedGuide] = useState();
+  const [history, setHistory] = useState([]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        {/** <img src={logo} className="App-logo" alt="logo" /> */}
+      <header className="AppHeader">
+        {history.length == 0 ? null
+          : <div className="BackButton" onClick={onBack}>
+            <svg aria-label="Back" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Back</title><path d="M21 17.502a.997.997 0 0 1-.707-.293L12 8.913l-8.293 8.296a1 1 0 1 1-1.414-1.414l9-9.004a1.03 1.03 0 0 1 1.414 0l9 9.004A1 1 0 0 1 21 17.502Z"></path></svg>
+          </div>
+        }
         <p>
           {selectedGuide == null ? "My guides" : selectedGuide.title}
         </p>
+        {history.length == 0
+          ? null
+          : <div className="SpaceFillter" />}
       </header>
       {selectedGuide == null
         ? <div className="GuidesList">
           {guides.map(guide => (
-            <GuideTile key={`guide_${guide.id}`} guide={guide} onClick={onClick}></GuideTile>
+            <GuideTile key={`guide_${guide.id}`} guide={guide} onClick={onItemSelected}></GuideTile>
           ))}
         </div>
         : <GuideView guide={selectedGuide} />}
--- src/views/PostTile.js
+++ src/views/PostTile.js
@@ -24,7 +24,7 @@ function PostTile({ guide, post }) {
             </div>
             {post.type == 'reel' &&
                 <div className="ReelMarkWrapper">
-                    <svg aria-label="Video" class="x1lliihq x1n2onr6 x1hfr7tm x9bdzbf" fill="currentColor" height="18" role="img" viewBox="0 0 24 24" width="18"><title>Video</title><path d="M5.888 22.5a3.46 3.46 0 0 1-1.721-.46l-.003-.002a3.451 3.451 0 0 1-1.72-2.982V4.943a3.445 3.445 0 0 1 5.163-2.987l12.226 7.059a3.444 3.444 0 0 1-.001 5.967l-12.22 7.056a3.462 3.462 0 0 1-1.724.462Z"></path></svg>
+                    <svg aria-label="Video" fill="currentColor" height="18" role="img" viewBox="0 0 24 24" width="18"><title>Video</title><path d="M5.888 22.5a3.46 3.46 0 0 1-1.721-.46l-.003-.002a3.451 3.451 0 0 1-1.72-2.982V4.943a3.445 3.445 0 0 1 5.163-2.987l12.226 7.059a3.444 3.444 0 0 1-.001 5.967l-12.22 7.056a3.462 3.462 0 0 1-1.724.462Z"></path></svg>
                 </div>
             }
 
