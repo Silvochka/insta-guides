d78b7be5a688b37c05f5704caadf88aecc310efc
Evgenia Filatova <silvochka07@gmail.com>
2023-12-17 15:17:51 -0800

    re-org wa nature

--- src/guides.js
+++ src/guides.js
@@ -495,6 +495,123 @@ export const guides = [
     {
         "id": "wa-nature",
         "title": "Природа Вашингтона",
+        "guides": [{
+            "id": "wa-nature/olympic-peninsula",
+            "title": "Washington Olympic Peninsula",
+            "posts": [{
+                "id": 1,
+                "title": "Port Angeles зимой 2020",
+                "link": "instagram://media?id=2257884882869176609_3099591977",
+            }, {
+                "id": 2,
+                "title": "Ruby beach",
+                "link": "instagram://media?id=2405828433321332659_3099591977",
+            }, {
+                "id": 3,
+                "title": "Hoh Rainforest",
+                "link": "instagram://media?id=2403637266206836461_3099591977",
+            }, {
+                "id": 4,
+                "title": "Rialto beach",
+                "link": "instagram://media?id=2400700006096223128_3099591977",
+            }, {
+                "id": 5,
+                "title": "Sol Duc Falls",
+                "link": "instagram://media?id=2398551872977006738_3099591977",
+            }, {
+                "id": 6,
+                "title": "Hurricane Ridge",
+                "link": "instagram://media?id=2395638078328143473_3099591977",
+            }, {
+                "id": 7,
+                "title": "Abeerden",
+                "link": "instagram://media?id=2279533862128620186_3099591977",
+            }, {
+                "id": 8,
+                "title": "Crescent Lake",
+                "link": "instagram://media?id=2117100909862736119_3099591977",
+            }, {
+                "id": 9,
+                "title": "Sequim",
+                "link": "instagram://media?id=2111367600230787763_3099591977",
+            }, {
+                "id": 10,
+                "title": "Port Angeles летом 2019",
+                "link": "instagram://media?id=2076328372795329069_3099591977",
+            }, {
+                "id": 11,
+                "title": "Port Angeles, март 2019",
+                "link": "instagram://media?id=2008480717101035862_3099591977",
+            },]
+        }, {
+            "id": "wa-nature/i-90",
+            "title": "Along I-90",
+            "posts": [{
+                "id": 1,
+                "title": "Little Si",
+                "link": "instagram://media?id=2081638418500344392_3099591977",
+            },  {
+                "id": 2,
+                "title": "Twin Falls",
+                "link": "instagram://media?id=2060828982747995741_3099591977",
+            }, {
+                "id": 4,
+                "title": "Rattlesnake ridge",
+                "link": "instagram://media?id=1972302517723602784_3099591977",
+            }, {
+                "id": 5,
+                "title": "Snoqualmie falls",
+                "link": "instagram://media?id=1963008657713376543_3099591977",
+            },  {
+                "id": 6,
+                "title": "Poo Poo Point",
+                "link": "instagram://media?id=2809493709395608313_3099591977",
+            }, {
+                "id": 3,
+                "title": "Mount Margaret",
+                "link": "instagram://media?id=2000609485726784209_3099591977",
+            }, ]
+        }, {
+            "id": "wa-nature/rainier",
+            "title": "Nearby Rainier",
+            "posts": [{
+                "id": 1,
+                "title": "Skyline trail, Mt Rainier",
+                "link": "instagram://media?id=2724695032646644144_3099591977",
+            }, {
+                "id": 2,
+                "title": "Crystal Mountain",
+                "link": "instagram://media?id=2141979526128062880_3099591977",
+            },  {
+                "id": 3,
+                "title": "Tipsoo lake",
+                "link": "instagram://media?id=2073709974504545568_3099591977",
+            },]
+        }, {
+            "id": "wa-nature/islands",
+            "title": "Islands",
+            "posts": [{
+                "id": 1,
+                "title": "San Juan island",
+                "link": "instagram://media?id=2573216039331696033_3099591977",
+            }, {
+                "id": 2,
+                "title": "Orcas Island",
+                "link": "instagram://media?id=2563068936445015349_3099591977",
+            }, {
+                "id": 5,
+                "title": "Vashon Island",
+                "link": "instagram://media?id=2314488199501999970_3099591977",
+            }, {
+                "id": 3,
+                "title": "Whidbey island",
+                "link": "instagram://media?id=2025837162305891992_3099591977",
+            }, {
+                "id": 4,
+                "title": "Bainbridge Island",
+                "link": "instagram://media?id=1554874688923063989_3099591977",
+            },]
+        }],
         "posts": [{
             "id": 1,
             "title": "East Washington",
@@ -504,14 +621,6 @@ export const guides = [
             "title": "Diablo Lake",
             "link": "instagram://media?id=2992764014522305507_3099591977",
         }, {
-            "id": 3,
-            "title": "Poo Poo Point",
-            "link": "instagram://media?id=2809493709395608313_3099591977",
-        }, {
-            "id": 4,
-            "title": "Skyline trail, Mt Rainier",
-            "link": "instagram://media?id=2724695032646644144_3099591977",
-        }, {
             "id": 5,
             "title": "Scenic park at Hood Canal",
             "link": "instagram://media?id=2707221879010612995_3099591977",
@@ -519,14 +628,6 @@ export const guides = [
             "id": 6,
             "title": "Северные Каскадные горы ‑ озеро Shannon",
             "link": "instagram://media?id=2627589139231025390_3099591977",
-        }, {
-            "id": 7,
-            "title": "San Juan island",
-            "link": "instagram://media?id=2573216039331696033_3099591977",
-        }, {
-            "id": 8,
-            "title": "Orcas Island",
-            "link": "instagram://media?id=2563068936445015349_3099591977",
         }, {
             "id": 9,
             "title": "Mount St Helens",
@@ -536,10 +637,6 @@ export const guides = [
             "title": "Fairhaven",
             "link": "instagram://media?id=2421301086978900413_3099591977",
         }, {
-            "id": 11,
-            "title": "Vashon Island",
-            "link": "instagram://media?id=2314488199501999970_3099591977",
-        }, {
             "id": 12,
             "title": "Pacific Ocean",
             "link": "instagram://media?id=2279533862128620186_3099591977",
@@ -547,10 +644,6 @@ export const guides = [
             "id": 13,
             "title": "Vantage",
             "link": "instagram://media?id=2228248014547088418_3099591977",
-        }, {
-            "id": 14,
-            "title": "Crystal Mountain",
-            "link": "instagram://media?id=2141979526128062880_3099591977",
         }, {
             "id": 15,
             "title": "Mukilteo",
@@ -560,46 +653,14 @@ export const guides = [
             "title": "Anacortes",
             "link": "instagram://media?id=2095513669751787084_3099591977",
         }, {
-            "id": 17,
-            "title": "Little Si",
-            "link": "instagram://media?id=2081638418500344392_3099591977",
-        }, {
-            "id": 18,
-            "title": "Tipsoo lake",
-            "link": "instagram://media?id=2073709974504545568_3099591977",
-        }, {
             "id": 19,
             "title": "Silverdale",
             "link": "instagram://media?id=2065898167618396915_3099591977",
         }, {
-            "id": 20,
-            "title": "Twin Falls",
-            "link": "instagram://media?id=2060828982747995741_3099591977",
-        }, {
             "id": 21,
             "title": "Skagit Valley",
             "link": "instagram://media?id=2028920427338065193_3099591977",
-        }, {
-            "id": 22,
-            "title": "Whidbey island",
-            "link": "instagram://media?id=2025837162305891992_3099591977",
-        }, {
-            "id": 23,
-            "title": "Mount Margaret",
-            "link": "instagram://media?id=2000609485726784209_3099591977",
-        }, {
-            "id": 24,
-            "title": "Rattlesnake ridge",
-            "link": "instagram://media?id=1972302517723602784_3099591977",
-        }, {
-            "id": 25,
-            "title": "Snoqualmie falls",
-            "link": "instagram://media?id=1963008657713376543_3099591977",
-        }, {
-            "id": 26,
-            "title": "Bainbridge Island",
-            "link": "instagram://media?id=1554874688923063989_3099591977",
-        },]
+        }, ]
     },
     {
         "id": "new-england",
@@ -936,55 +997,6 @@ export const guides = [
         "id": "wa",
         "title": "Washington",
         "posts": [{}],
-    },
-    {
-        "id": "wa-olympic-peninsula",
-        "title": "Washington Olympic Peninsula",
-        "posts": [{
-            "id": 1,
-            "title": "Port Angeles зимой 2020",
-            "link": "instagram://media?id=2257884882869176609_3099591977",
-        }, {
-            "id": 2,
-            "title": "Ruby beach",
-            "link": "instagram://media?id=2405828433321332659_3099591977",
-        }, {
-            "id": 3,
-            "title": "Hoh Rainforest",
-            "link": "instagram://media?id=2403637266206836461_3099591977",
-        }, {
-            "id": 4,
-            "title": "Rialto beach",
-            "link": "instagram://media?id=2400700006096223128_3099591977",
-        }, {
-            "id": 5,
-            "title": "Sol Duc Falls",
-            "link": "instagram://media?id=2398551872977006738_3099591977",
-        }, {
-            "id": 6,
-            "title": "Hurricane Ridge",
-            "link": "instagram://media?id=2395638078328143473_3099591977",
-        }, {
-            "id": 7,
-            "title": "Abeerden",
-            "link": "instagram://media?id=2279533862128620186_3099591977",
-        }, {
-            "id": 8,
-            "title": "Crescent Lake",
-            "link": "instagram://media?id=2117100909862736119_3099591977",
-        }, {
-            "id": 9,
-            "title": "Sequim",
-            "link": "instagram://media?id=2111367600230787763_3099591977",
-        }, {
-            "id": 10,
-            "title": "Port Angeles летом 2019",
-            "link": "instagram://media?id=2076328372795329069_3099591977",
-        }, {
-            "id": 11,
-            "title": "Port Angeles, март 2019",
-            "link": "instagram://media?id=2008480717101035862_3099591977",
-        },]
     },
     {
         "id": "road-trips",
